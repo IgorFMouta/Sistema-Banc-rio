74b6a4df636364ab2deda14cde1e7710c5078e55
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-10-15 14:29:44 -0300

    Alteração...

--- banco-residencia/src/br/com/residencia/IO/LeituraEscrita.java
+++ banco-residencia/src/br/com/residencia/IO/LeituraEscrita.java
@@ -2,6 +2,7 @@ package br.com.residencia.IO;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,13 +10,31 @@ import java.util.Scanner;
 
 public class LeituraEscrita {
 
-	final static String PATH_BASICO = "./temp/";
-	final static String EXTENSAO =".txt";
-	
+	static final String PATH_BASICO = "./temp/";
+	final static String EXTENSAO = ".txt";
+
 	public static void leitor(String path) throws IOException {
-	
 		
-		BufferedReader buffRead = new BufferedReader(new FileReader(PATH_BASICO + path));
-	
+		try {
+			BufferedReader buffRead = new BufferedReader(new FileReader(PATH_BASICO + path));
+			//String linha = "";
+			
+			while (true) {	
+				String linha = buffRead.readLine();
+				if (linha != null) {
+					String[] info = linha.split(";");
+					//ENUM
+					System.out.println(info[0] + "\n" + info[1] + "\n" + info[2] + "\n" + info[3] + "\n" 
+					+ info[4] + "\n" + info[5]);
+				} else {
+					break;					
+				}
+			}
+			buffRead.close();
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado no local especificado " + PATH_BASICO + path);
+		}
+
 	}
 }
