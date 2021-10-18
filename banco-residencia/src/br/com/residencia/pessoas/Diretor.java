12869b1a48f50b26e349c7592e4b13ff9ccb423a
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-10-18 15:38:59 -0300

    Atualização da Classe Diretor 18/10 15:38

--- banco-residencia/src/br/com/residencia/pessoas/Diretor.java
+++ banco-residencia/src/br/com/residencia/pessoas/Diretor.java
@@ -1,54 +1,22 @@
 package br.com.residencia.pessoas;
 
-public class Diretor extends Gerente {
-	
-	//a. Relatório com as informações de Nome, CPF e Agência de todos os clientes do sistema em ordem alfabética.
-	
-	
-	
-		public void nomesEmOrdem(String[] args) {
-			//nome dos clientes
-			//String[] nomes = ordenaArray(new String[]{"Fulano", "Sicrano", "Beltrano", "Barabara", "Samy", "Fabiana" });
-		
-			
-			
-			
-			
-			/*Imprime os nomes
-			for (int i = 0; i < nomes.length; i++) {
-				System.out.println(nomes[i]);
-			}*/
+	public class Diretor extends Funcionario {
+
+		public Diretor() {
+			super();
 		}
-		public String[] ordenaArray(String[] arrayNomes){
-			
-			String aux = "";
-			
-			for (int i = 0; i < arrayNomes.length; i++) {
-				
-				for (int j = 0; j < arrayNomes.length; j++) {
-					String nome1 = arrayNomes[i];
-					String nome2 = arrayNomes[j];
-					
-					if(nome2.charAt(0) > nome1.charAt(0)){
-						aux = nome1; 
-						arrayNomes[i] = nome2;
-						arrayNomes[j] = aux;
-					
-					}else if(nome2.charAt(0) == nome1.charAt(0)){
-						if(nome2.charAt(1) > nome1.charAt(1)){
-							aux = nome1;
-							arrayNomes[i] = nome2;
-							arrayNomes[j] = aux;
-						}	
-						
-					}
-				}
-		
-			}
 
-			return arrayNomes;
+		public Diretor(String tipoUsuarios, String nome, String sobrenome, String cpf, String senha, Integer agencia,
+				Integer numeroConta, Double saldo, Double salario) {
+			super(tipoUsuarios, nome, sobrenome, cpf, senha, agencia, numeroConta, saldo, salario);
+		}
+
+		@Override
+		public String toString() {
+			return "Diretor [salario=" + this.salario + ", nome=" + this.nome
+					+ ", tipoUsuario=" + this.tipoUsuarios + ", cpf=" + this.cpf + ", senha=" + this.senha + ", agencia="
+					+ this.agencia + ", numeroConta=" + this.numeroConta + "]\n";
+		}
+
 	}
-}
-
-
 
